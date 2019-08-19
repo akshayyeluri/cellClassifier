114fe0579c6019dcdcff37a45dc64084e6d86f0c
Akshay Yeluri <yeluri@deshawresearch.com>
2019-08-19 14:53:27 -0400

    Added architecture for data with all zero columns removed

---
+++ arch_relevant_cols.py
@@ -0,0 +1,81 @@
+import tensorflow as tf
+import numpy as np
+
+WIDTH = 19232
+HEIGHT = 1
+DEPTH = 1
+SEED = 13
+
+
+def reshape_data(data):
+    return data.reshape(-1, WIDTH, HEIGHT, DEPTH)
+
+
+def make_vars_on_cpu(NCLASS):
+    with tf.compat.v1.variable_scope(__name__):
+        tf.compat.v1.get_variable(name="conv0_w", \
+          initializer=tf.random.truncated_normal([5, 1, DEPTH, 16], stddev=0.1, seed=SEED))
+        tf.compat.v1.get_variable(name="conv0_b", initializer=tf.zeros([16]))
+        
+        tf.compat.v1.get_variable(name="conv1_w", \
+          initializer=tf.random.truncated_normal([5, 1, 16, 16], stddev=0.1, seed=SEED))
+        tf.compat.v1.get_variable(name="conv1_b", initializer=tf.constant(0.1, shape=[16]))
+        
+        tf.compat.v1.get_variable(name="conv2_w", \
+          initializer=tf.random.truncated_normal([5, 1, 16, 32], stddev=0.1, seed=SEED))
+        tf.compat.v1.get_variable(name="conv2_b", initializer=tf.constant(0.1, shape=[32]))
+
+        tf.compat.v1.get_variable(name="conv3_w", \
+          initializer=tf.random.truncated_normal([2, 1, 32, 32], stddev=0.1, seed=SEED))
+        tf.compat.v1.get_variable(name="conv3_b", initializer=tf.constant(0.1, shape=[32]))
+
+        tf.compat.v1.get_variable(name="conv4_w", \
+          initializer=tf.random.truncated_normal([5, 1, 32, 64], stddev=0.1, seed=SEED))
+        tf.compat.v1.get_variable(name="conv4_b", initializer=tf.constant(0.1, shape=[64]))
+
+        tf.compat.v1.get_variable(name="fc1_w", \
+          initializer=tf.random.truncated_normal([50 * 64, 512], stddev=0.1, seed=SEED))
+        tf.compat.v1.get_variable(name="fc1_b", initializer=tf.constant(0.1, shape=[512]))
+        
+        tf.compat.v1.get_variable(name="fc2_w", \
+          initializer=tf.random.truncated_normal([512, NCLASS], stddev=0.1, seed=SEED))
+        tf.compat.v1.get_variable(name="fc2_b", initializer=tf.constant(0.1, shape=[NCLASS]))
+
+
+def model(inp, train=False):
+    # Params
+    NCONV = 5
+    pads = ["SAME", "SAME", "SAME", "VALID", "SAME"]
+    pool_ks = [2, 4, 4, 4, 3]
+
+    # Get the variables
+    conv_ws, conv_bs = [], []
+    with tf.compat.v1.variable_scope(__name__, reuse=True):
+        for i in range(NCONV):
+            conv_ws.append(tf.compat.v1.get_variable(f"conv{i}_w"))
+            conv_bs.append(tf.compat.v1.get_variable(f"conv{i}_b"))
+        fc1_weights = tf.compat.v1.get_variable("fc1_w")
+        fc1_biases = tf.compat.v1.get_variable("fc1_b")
+        fc2_weights = tf.compat.v1.get_variable("fc2_w")
+        fc2_biases = tf.compat.v1.get_variable("fc2_b")
+
+    # Wire them into a convolutional neural net
+    pool = inp
+    for i in range(NCONV):
+        conv = tf.nn.conv2d(pool, conv_ws[i], strides=[1,1,1,1], padding=pads[i])
+        relu = tf.nn.relu(tf.nn.bias_add(conv, conv_bs[i]))
+        pool = tf.nn.max_pool(relu, ksize=[1, pool_ks[i], 1, 1], \
+                           strides=[1, pool_ks[i], 1, 1], padding="VALID")
+
+    size = pool.get_shape().as_list()
+    pool_flat = tf.reshape(pool, [size[0], size[1] * size[2] * size[3]])
+    dense1 = tf.nn.relu(tf.matmul(pool_flat, fc1_weights) + fc1_biases)
+    if train:
+        # Add L2 regularization and dropout
+        dense1 = tf.nn.dropout(dense1, rate=0.5, seed=SEED)
+        regularizers = (tf.nn.l2_loss(fc1_weights) + tf.nn.l2_loss(fc1_biases)+\
+                        tf.nn.l2_loss(fc2_weights) + tf.nn.l2_loss(fc2_biases))
+        regularizers = 5e-4 * regularizers
+        
+    dense2 = tf.nn.bias_add(tf.matmul(dense1, fc2_weights), fc2_biases)
+    return (dense2, regularizers) if train else dense2
