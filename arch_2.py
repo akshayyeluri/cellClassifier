3d12ad2755c0c434e0c8618b5152b3ab985847e1
Akshay Yeluri <yeluri@deshawresearch.com>
2019-08-19 14:27:31 -0400

    Cleaned up architecture file to be more scalable for more conv layers

--- arch_2.py
+++ arch_2.py
@@ -13,17 +13,17 @@ def reshape_data(data):
 
 def make_vars_on_cpu(NCLASS):
     with tf.compat.v1.variable_scope(__name__):
-        tf.compat.v1.get_variable(name="conv1_w", \
+        tf.compat.v1.get_variable(name="conv0_w", \
           initializer=tf.random.truncated_normal([3, 1, DEPTH, 32], stddev=0.1, seed=SEED))
-        tf.compat.v1.get_variable(name="conv1_b", initializer=tf.zeros([32]))
+        tf.compat.v1.get_variable(name="conv0_b", initializer=tf.zeros([32]))
+        
+        tf.compat.v1.get_variable(name="conv1_w", \
+          initializer=tf.random.truncated_normal([3, 1, 32, 64], stddev=0.1, seed=SEED))
+        tf.compat.v1.get_variable(name="conv1_b", initializer=tf.constant(0.1, shape=[64]))
         
         tf.compat.v1.get_variable(name="conv2_w", \
-          initializer=tf.random.truncated_normal([3, 1, 32, 64], stddev=0.1, seed=SEED))
-        tf.compat.v1.get_variable(name="conv2_b", initializer=tf.constant(0.1, shape=[64]))
-        
-        tf.compat.v1.get_variable(name="conv3_w", \
           initializer=tf.random.truncated_normal([2, 1, 64, 128], stddev=0.1, seed=SEED))
-        tf.compat.v1.get_variable(name="conv3_b", initializer=tf.constant(0.1, shape=[128]))
+        tf.compat.v1.get_variable(name="conv2_b", initializer=tf.constant(0.1, shape=[128]))
 
         tf.compat.v1.get_variable(name="fc1_w", \
           initializer=tf.random.truncated_normal([34 * 128, 512], stddev=0.1, seed=SEED))
@@ -35,35 +35,33 @@ def make_vars_on_cpu(NCLASS):
 
 
 def model(inp, train=False):
+    # Params
+    NCONV = 3
+    pads = ["VALID", "SAME", "VALID"]
+    pool_ks = [6, 16, 10]
+
     # Get the variables
+    conv_ws, conv_bs = [], []
     with tf.compat.v1.variable_scope(__name__, reuse=True):
-        conv1_weights = tf.compat.v1.get_variable("conv1_w")
-        conv1_biases = tf.compat.v1.get_variable("conv1_b")
-        conv2_weights = tf.compat.v1.get_variable("conv2_w")
-        conv2_biases = tf.compat.v1.get_variable("conv2_b")
-        conv3_weights = tf.compat.v1.get_variable("conv3_w")
-        conv3_biases = tf.compat.v1.get_variable("conv3_b")
+        for i in range(NCONV):
+            conv_ws.append(tf.compat.v1.get_variable(f"conv{i}_w"))
+            conv_bs.append(tf.compat.v1.get_variable(f"conv{i}_b"))
         fc1_weights = tf.compat.v1.get_variable("fc1_w")
         fc1_biases = tf.compat.v1.get_variable("fc1_b")
         fc2_weights = tf.compat.v1.get_variable("fc2_w")
         fc2_biases = tf.compat.v1.get_variable("fc2_b")
 
     # Wire them into a convolutional neural net
-    conv1 = tf.nn.conv2d(inp, conv1_weights, strides=[1,1,1,1], padding="VALID")
-    relu1 = tf.nn.relu(tf.nn.bias_add(conv1, conv1_biases))
-    pool1 = tf.nn.max_pool(relu1, ksize=[1, 6, 1, 1], \
-                           strides=[1, 6, 1, 1], padding="VALID")
-    conv2 = tf.nn.conv2d(pool1, conv2_weights, strides=[1,1,1,1], padding="SAME")
-    relu2 = tf.nn.relu(tf.nn.bias_add(conv2, conv2_biases))
-    pool2 = tf.nn.max_pool(relu2, ksize=[1, 16, 1, 1], \
-                           strides=[1, 16, 1, 1], padding="VALID")
-    conv3 = tf.nn.conv2d(pool2, conv3_weights, strides=[1,1,1,1], padding="VALID")
-    relu3 = tf.nn.relu(tf.nn.bias_add(conv3, conv3_biases))
-    pool3 = tf.nn.max_pool(relu3, ksize=[1, 10, 1, 1], \
-                           strides=[1, 10, 1, 1], padding="VALID")
-    size = pool3.get_shape().as_list()
-    pool3_flat = tf.reshape(pool3, [size[0], size[1] * size[2] * size[3]])
-    dense1 = tf.nn.relu(tf.matmul(pool3_flat, fc1_weights) + fc1_biases)
+    pool = inp
+    for i in range(NCONV):
+        conv = tf.nn.conv2d(pool, conv_ws[i], strides=[1,1,1,1], padding=pads[i])
+        relu = tf.nn.relu(tf.nn.bias_add(conv, conv_bs[i]))
+        pool = tf.nn.max_pool(relu, ksize=[1, pool_ks[i], 1, 1], \
+                           strides=[1, pool_ks[i], 1, 1], padding="VALID")
+
+    size = pool.get_shape().as_list()
+    pool_flat = tf.reshape(pool, [size[0], size[1] * size[2] * size[3]])
+    dense1 = tf.nn.relu(tf.matmul(pool_flat, fc1_weights) + fc1_biases)
     if train:
         # Add L2 regularization and dropout
         dense1 = tf.nn.dropout(dense1, rate=0.5, seed=SEED)
